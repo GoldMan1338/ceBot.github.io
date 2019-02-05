bed6cda3652bdae1500906bcf7057cc9cd937162
Chipbonk <cody.perkins9@gmail.com>
2019-02-04 22:07:54 -0700

    fuck

--- CE-Bot/src/main/java/io/github/ceBot/CommandHandler.java
+++ CE-Bot/src/main/java/io/github/ceBot/CommandHandler.java
@@ -37,18 +37,20 @@ public class CommandHandler {
     	commandMap.put("alterpic", (event, args) -> {
 			String imstring = args.get(0).toString();
         	MainRunner.sendMessage(event.getChannel(), imstring);
-    		try {
-    			URL imurl = new URL(imstring);
-    			BufferedImage img = ImageIO.read(imurl);
-    			File f = new File("temp");
-    			ImageIO.write(img, "", f);
-    			Image im = (Image) ImageIO.read(f);
-    			Main.bot.changeAvatar(im);
+        	if(event.getAuthor().getStringID().contains(Main.BOT_OWNER[0]) || event.getAuthor().getStringID().contains(Main.BOT_OWNER[1])){
+    			try {
+    				URL imurl = new URL(imstring);
+    				BufferedImage img = ImageIO.read(imurl);
+    				File f = new File("temp");
+    				ImageIO.write(img, "", f);
+    				Image im = (Image) ImageIO.read(f);
+    				Main.bot.changeAvatar(im);
+    			}
+    				catch (IOException e) {
+				 	// TODO Auto-generated catch block
+				 	e.printStackTrace();
+			 	}
     		}
-			 catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
     	});
     	commandMap.put("shutdown", (event, args) -> {
 
