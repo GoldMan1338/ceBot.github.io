06235ff3d26b14175914766995aba9e08cea9d58
Chipbonk <cody.perkins9@gmail.com>
2019-02-11 12:22:34 -0700

    Bot can always shutdown now

--- CE-Bot/src/main/java/io/github/ceBot/Main.java
+++ CE-Bot/src/main/java/io/github/ceBot/Main.java
@@ -1,13 +1,17 @@
 package io.github.ceBot;
 
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
-
+import discord4j.core.object.util.Snowflake;
 import io.github.ceBot.*;
 
 public class Main {
@@ -27,16 +31,16 @@ public class Main {
 		
 		client.getEventDispatcher().on(MessageCreateEvent.class)
     		.map(MessageCreateEvent::getMessage)
-    		.filterWhen(message -> message.getAuthor().map(user -> !user.isBot()))
-    		.filter(message -> message.getContent().orElse("").equalsIgnoreCase(">ping"))
+    		.filterWhen(message -> message.getAuthorAsMember().map(user -> !user.isBot()))
+    		.filter(message -> message.getContent().orElse("").equalsIgnoreCase(">>ping"))
     		.flatMap(Message::getChannel)
     		.flatMap(channel -> channel.createMessage("Pong!"))
     		.subscribe();
 		//this is to make sure that we can always turn it off
 		client.getEventDispatcher().on(MessageCreateEvent.class)
 			.map(MessageCreateEvent::getMessage)
-			.filterWhen(message -> message.getAuthor().map(user -> !user.isBot()))
-			.filter(message -> message.getAuthorId().toString().split(" ").equals(Main.BOT_OWNER))
+			.filterWhen(message -> message.getAuthorAsMember().map(user -> !user.isBot()))
+			.filter(event -> ownerIds.contains(event.getAuthor().map(User::getId).map(Snowflake::asLong).orElse(0l)))
 			.filter(message -> message.getContent().orElse("").equalsIgnoreCase(">>shutdown"))
 			.flatMap(Message::getChannel)
 			.flatMap(channel -> channel.createMessage("Ceasing to exist"))
@@ -51,12 +55,15 @@ public class Main {
 		
 		
 		
+		
 		client.login().block();
 	}
 	
 	public static final String BOT_PREFIX = ">";
 	
 	public static String[] BOT_OWNER = {"139541886888181760","422588424487174144","195621535703105536"};
+	
+	public static Set<Long> ownerIds = Stream.of(139541886888181760l,422588424487174144l,195621535703105536l).collect(Collectors.toSet());
 	
 	public static String BOT_INVITE = "https://discordapp.com/oauth2/authorize?client_id=542137435681718273&scope=bot&permissions=8";
 	
